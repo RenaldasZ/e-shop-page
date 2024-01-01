e8d2260cbce6dbffd973682b47717eb64de2fc86
ErnAbr <ern_abr@yahoo.com>
2024-01-01 15:02:42 +0200

    basic card

---
+++ frontend/src/components/Card/BlogCard.tsx
@@ -0,0 +1,38 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function BlogCard() {
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        border: "1px solid white",
+        marginLeft: "5px",
+        marginTop: "15px",
+      }}
+    >
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button size="small">Review</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
