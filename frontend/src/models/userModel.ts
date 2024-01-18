0baaa7d4d33e9a19a4239c9100f54da0d896ff66
ErnAbr <ern_abr@yahoo.com>
2024-01-18 22:47:17 +0200

    connection to the server established

---
+++ frontend/src/models/userModel.ts
@@ -0,0 +1,6 @@
+export interface User {
+    email: string,
+    is_staff: boolean,
+    url: string,
+    username: string
+}
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import agent from "../../api/agent";
+import { User } from "../../models/userModel";
 
 export default function Register() {
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState<null | User>(null);
 
   useEffect(() => {
     agent.Users.getUsers()
