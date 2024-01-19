a60ea17228639588f30d991ac528fd52d0840491
kobrius11 <kobrius11@gmail.com>
2024-01-19 18:49:01 +0200

    Logout api View, ApiViews tranfered to views.py

--- webshop/api/rest/router.py
+++ webshop/api/rest/router.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.models import User
-from rest_framework import routers, viewsets
+from rest_framework import routers
 from api.rest.viewset import UserViewSet, CustomerViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'Customers', CustomerViewSet)
+router.register(r'customers', CustomerViewSet)
