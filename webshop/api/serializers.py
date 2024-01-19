aaee5898fddc4720cc0d2a5d372937da78d84eaf
kobrius11 <kobrius11@gmail.com>
2024-01-19 10:58:14 +0200

    users validation

--- webshop/api/serializers.py
+++ webshop/api/serializers.py
@@ -1,8 +1,34 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'password', 'is_staff']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, value):
+        """
+        Validate password complexity.
+        """
+        try:
+            validate_password(value)
+        except serializers.ValidationError as exc:
+            raise serializers.ValidationError(str(exc))
+
+        return value
+    
+    def validate_email(self, value):
+        """
+        Check if the email already exists in the database.
+        """
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists. Please use a different email.")
+        return value
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
