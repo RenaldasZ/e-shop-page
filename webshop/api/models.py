09a001e3cd974cab72f4362685dd512f12a0a2c2
kobrius11 <kobrius11@gmail.com>
2024-01-20 17:57:53 +0200

    models.py Meta class placeholers added, paginator redundant get_page_size function fix

--- webshop/api/models.py
+++ webshop/api/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
 
+    class Meta:
+        verbose_name = ""
+        verbose_name_plural = ""
+        db_table_comment = "Question answers"
+        pass
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -14,12 +20,30 @@ class Product(models.Model):
     quantityInStock = models.IntegerField(default=5)
     productSize = models.CharField(max_length=100, default="m")
 
+    class Meta:
+        verbose_name = ""
+        verbose_name_plural = ""
+        db_table_comment = "Question answers"
+        pass
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='OrderItem')
     order_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = ""
+        verbose_name_plural = ""
+        db_table_comment = "Question answers"
+        pass
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = ""
+        verbose_name_plural = ""
+        db_table_comment = "Question answers"
+        pass
--- webshop/api/rest/paginator.py
+++ webshop/api/rest/paginator.py
@@ -1,4 +1,6 @@
-from rest_framework.pagination import PageNumberPagination, Response, OrderedDict
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from collections import OrderedDict
 
 class LargeResultsSetPagination(PageNumberPagination):
     page_size = 10
@@ -10,19 +12,11 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-    def get_page_size(self, request):
-        # Allow clients to set a custom page size using the 'page_size' query parameter
-        page_size = request.query_params.get('page_size', None)
-        if page_size:
-            return int(page_size)
-        return super().get_page_size(request)
-
     def get_paginated_response(self, data):
         return Response(OrderedDict([
             ('count', self.page.paginator.count),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
             ('current', self.page.number),
-            ('page_size', self.page_size),
             ('results', data)
         ]))
