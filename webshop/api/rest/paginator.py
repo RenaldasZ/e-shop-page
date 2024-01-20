a81fde4c336580e776232ad41ebfd1295d93e4af
kobrius11 <kobrius11@gmail.com>
2024-01-20 17:05:18 +0200

    products endpoint page_size url_param added

--- webshop/api/rest/paginator.py
+++ webshop/api/rest/paginator.py
@@ -6,9 +6,16 @@ class LargeResultsSetPagination(PageNumberPagination):
     max_page_size = 10
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 6
     page_size_query_param = 'page_size'
-    max_page_size = 5
+    max_page_size = 100
+
+    def get_page_size(self, request):
+        # Allow clients to set a custom page size using the 'page_size' query parameter
+        page_size = request.query_params.get('page_size', None)
+        if page_size:
+            return int(page_size)
+        return super().get_page_size(request)
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
@@ -16,5 +23,6 @@ class StandardResultsSetPagination(PageNumberPagination):
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
             ('current', self.page.number),
+            ('page_size', self.page_size),
             ('results', data)
         ]))
