7d0bbc9bc4bb2050c33e015eab21d18ac1b4fbb5
RenaldasZ <renald.zvega@gmail.com>
2024-01-21 17:23:35 +0200

    pagination bug fix

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -60,7 +60,7 @@ useEffect(() => {
       setFilteredProducts(tempProducts);
       setTotalCount(tempProducts.length / productsPerPage);
 
-      if (Math.floor(tempProducts.length / productsPerPage) === 0) {
+      if (Math.floor(tempProducts.length / (productsPerPage + 1 )) == 0) {
         setCurrentPage(1);
       }
     }
