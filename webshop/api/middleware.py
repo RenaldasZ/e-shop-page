871a2efeeca649e1ebb04b1501106cd012129be6
kobrius11 <kobrius11@gmail.com>
2024-01-23 16:53:05 +0200

    api.middleware added, only localhost can reach /api/

---
+++ webshop/api/middleware.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseForbidden
+
+class APILocalhostOnlyMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Check if the path starts with '/api/' and if the request is not from localhost
+        if request.path.startswith('/api/') and request.META['REMOTE_ADDR'] not in ['127.0.0.1', '::1']:
+            return HttpResponseForbidden("Access Forbidden to /api/ from this IP address")
+
+        return self.get_response(request)
--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'api.middleware.APILocalhostOnlyMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),#timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
