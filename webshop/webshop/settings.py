b2d164ac0450c7ce153e5def55a3517af50c66fb
kobrius11 <kobrius11@gmail.com>
2024-02-05 15:54:05 +0200

    Fronten:  changes in main, Login

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -309,3 +309,29 @@ RATE_LIMITS = {
     "signup": "20/m",
     # NOTE: Login is already protected via `ACCOUNT_LOGIN_ATTEMPTS_LIMIT`
 }
+
+SOCIALACCOUNT_PROVIDERS = {
+"google": {
+    # For each OAuth based provider, either add a ``SocialApp``
+    # (``socialaccount`` app) containing the required client
+    # credentials, or list them here:
+    "APP": {
+        "client_id": local_settings.GOOGLE_CLIENT_ID,
+        "secret": local_settings.GOOGLE_SECRET_KEY,
+        "key": ""
+    },
+    # These are provider-specific settings that can only be
+    # listed here:
+    "SCOPE": [
+        "profile",
+        "email",
+    ],
+    "AUTH_PARAMS": {
+        "access_type": "online",
+    }
+}}
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
