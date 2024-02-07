0d35a05d629c56101571051978ff3177d677688b
ErnAbr <ern_abr@yahoo.com>
2024-02-07 21:43:48 +0200

    new login error handling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-const cssFileName: string = 'index.min.css'
+const cssFileName: string = "index.min.css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,18 +10,17 @@ export default defineConfig({
   },
   plugins: [react()],
 
-  publicDir: './public',
+  publicDir: "./public",
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: (file) => {
-          return `assets/css/${cssFileName}`
+        assetFileNames: () => {
+          return `assets/css/${cssFileName}`;
         },
-        entryFileNames: (file) => {
-          return `assets/js/[name].min.js`
-        }
-      }
-    }
-  }
-
-})
+        entryFileNames: () => {
+          return `assets/js/[name].min.js`;
+        },
+      },
+    },
+  },
+});
